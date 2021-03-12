d5c7246553faae42c564f79fbee4f66a9a35114f
Jeremy Briones <jeremybr99@gmail.com>
2021-03-11 20:48:03 -0600

    creacion tabala de estacionamientos en DB

--- Models/Estacionamiento.cs
+++ Models/Estacionamiento.cs
@@ -2,14 +2,16 @@ namespace ElParqueito.Models
 {
     public class Estacionamiento
     {
+        public int Id{get;set;}
         public int HoraDeEntrada{get;set;}
         public int numeroPosicion{get;set;}
         public Vehiculo vehiculo{get;set;}
         public int HoraDeSalida{get;set;}
         public int Cobro{get;set;}
 
-        public Estacionamiento(int nuevaHoraDeEntra, int nuevoNumeroPosicion, Vehiculo nuevoVehiculo, int nuevaHoraDeSalida, int nuevoCobro)
+        public Estacionamiento(int nuevoId, int nuevaHoraDeEntra, int nuevoNumeroPosicion, Vehiculo nuevoVehiculo, int nuevaHoraDeSalida, int nuevoCobro)
         {
+            Id = nuevoId;
             HoraDeEntrada=nuevaHoraDeEntra;
             numeroPosicion=nuevoNumeroPosicion;
             vehiculo=nuevoVehiculo;
