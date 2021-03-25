ad2569a5fbb2877103d852f1008bb47a7e849a1d
chalomd8 <gonzalomd14@gmail.com>
2021-03-24 18:53:54 -0600

    probandoFacturaa

---
+++ Migrations/20210325004106_factura.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ElParqueito.Migrations
+{
+    public partial class factura : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
