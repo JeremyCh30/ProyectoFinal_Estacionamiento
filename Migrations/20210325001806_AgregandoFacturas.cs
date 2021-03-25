18028149ace089621c987a02638a581ec7688a94
Andony Nuñez <andony91@gmail.com>
2021-03-24 18:25:08 -0600

    Agregando migracion faltante

---
+++ Migrations/20210325001806_AgregandoFacturas.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ElParqueito.Migrations
+{
+    public partial class AgregandoFacturas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "facturas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Telefono = table.Column<string>(type: "TEXT", nullable: true),
+                    estacionamientoId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_facturas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_facturas_Estacionamientos_estacionamientoId",
+                        column: x => x.estacionamientoId,
+                        principalTable: "Estacionamientos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_facturas_estacionamientoId",
+                table: "facturas",
+                column: "estacionamientoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "facturas");
+        }
+    }
+}
--- Repositories/FacturaReporsitories.cs
+++ Repositories/FacturaReporsitories.cs
@@ -5,34 +5,33 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ElParqueito.Repositories
 {
-    public class FacturasRepositories 
+    public class FacturasRepositories
     {
         private CadenaDeParqueosContext db;
         public FacturasRepositories()
         {
             db = new CadenaDeParqueosContext();
         }
-         public List<Factura> ObtenerFacturas()
+        public List<Factura> ObtenerFacturas()
         {
-        var estacionamiento = db.facturas.Include(y=>y.estacionamiento).ToList();
-        db.SaveChanges();
-        return estacionamiento;
+            var estacionamiento = db.facturas.Include(y => y.estacionamiento).ThenInclude(z => z.vehiculo).ToList();
+            return estacionamiento;
         }
 
         public Factura ObtenerFactura(int id)
         {
-            var resultado= db.facturas.Find(id);
+            var resultado  = db.facturas.Find(id);
             db.SaveChanges();
             return resultado;
         }
 
 
-         public int AgregarFactura(Factura nuevaFactura)
-         {
-             var resultado =db.facturas.Add(nuevaFactura);
-             db.SaveChanges();
-             return resultado.Entity.Id;
-         }
-        
-    }   
+       public int AgregarFactura(Factura nuevaFactura)
+        {
+           var resultado =  db.facturas.Add(nuevaFactura);
+           db.SaveChanges();
+           return resultado.Entity.Id;
+        }
+
+    }
 }
