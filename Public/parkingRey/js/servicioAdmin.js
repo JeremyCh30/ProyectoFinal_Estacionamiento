6a04f1790ff60d3efbb256422dbd80803cdcd8cc
Jeremy Briones <jeremybr99@gmail.com>
2021-04-19 20:12:41 -0600

    modicacion de la visualizacion traida de la base de datos

--- Public/parkingRey/js/servicioAdmin.js
+++ Public/parkingRey/js/servicioAdmin.js
@@ -22,46 +22,57 @@ async function getEstacionamientos() {
                 let celdaPlaca = fila.insertCell();
                 let celdaTipo = fila.insertCell();
                 let celdaAcciones = fila.insertCell();
+                
+                if(resultados.vehiculo.placa!=""){
+                    celdaLugar.innerHTML = resultados.id;
+                    celdaPlaca.innerHTML = resultados.vehiculo.placa;
+                    celdaTipo.innerHTML = resultados.vehiculo.tipo;
 
-                celdaLugar.innerHTML = resultados.id;
-                celdaPlaca.innerHTML = resultados.vehiculo.placa;
-                celdaTipo.innerHTML = resultados.vehiculo.tipo;
+                    //inicio creacion de boton de eliminar
+                    let botonEliminar = document.createElement('a');
+                    //botonEliminar.href = '#';
+                    botonEliminar.classList.add('fas');
+                    botonEliminar.classList.add('fa-trash-alt');
+                    botonEliminar.classList.add('delete');
+                    botonEliminar.setAttribute("title", "Eliminar");
+                    botonEliminar.dataset.id_estacionamiento = resultados.id;
+                    //botonEliminar.setAttribute("onclick", "deleteEstacionamiento(" + resultados.id + ")");
+                    celdaAcciones.appendChild(botonEliminar);
+                    //fin creacion de boton de eliminar
+
+                    //inicio creacion de boton de Facturar
+                    let botonFacturar = document.createElement('a');
+                    botonFacturar.href = '#';
+                    botonFacturar.classList.add('fas');
+                    botonFacturar.classList.add('fa-dollar-sign');
+                    botonFacturar.classList.add('pay');
+                    botonFacturar.setAttribute("title", "Facturar");
+                    botonFacturar.dataset.id_estacionamiento = resultados.id;
+                    celdaAcciones.appendChild(botonFacturar);
+                    //fin creacion de boton de Facturar
+                }else{
+                    celdaLugar.innerHTML = resultados.id;
+                    celdaPlaca.innerHTML = "Disponible";
+                    // inicio boton Editar
+                    let botonEditar = document.createElement('a');
+                    //botonEditar.href = '#';
+                    botonEditar.classList.add('fas');
+                    botonEditar.classList.add('fa-pencil-alt'); //con esta clase se le dan los estilos en css
+                    botonEditar.classList.add('edit');
+                    botonEditar.setAttribute("title", "Agregar");
+                    botonEditar.dataset.id_estacionamiento = resultados.id;
+                    //botonEditar.addEventListener('click', mostrarDatosEdicion);
+                    celdaAcciones.appendChild(botonEditar); //editar
+                    // fin boton Editar
+                }
+                
 
 
-                // inicio boton Editar
-                let botonEditar = document.createElement('a');
-                //botonEditar.href = '#';
-                botonEditar.classList.add('fas');
-                botonEditar.classList.add('fa-pencil-alt'); //con esta clase se le dan los estilos en css
-                botonEditar.classList.add('edit');
-                botonEditar.setAttribute("title", "Editar");
-                botonEditar.dataset.id_estacionamiento = resultados.id;
-                //botonEditar.addEventListener('click', mostrarDatosEdicion);
-                celdaAcciones.appendChild(botonEditar); //editar
-                // fin boton Editar
+                
 
-                //inicio creacion de boton de eliminar
-                let botonEliminar = document.createElement('a');
-                //botonEliminar.href = '#';
-                botonEliminar.classList.add('fas');
-                botonEliminar.classList.add('fa-trash-alt');
-                botonEliminar.classList.add('delete');
-                botonEliminar.setAttribute("title", "Eliminar");
-                botonEliminar.dataset.id_estacionamiento = resultados.id;
-                //botonEliminar.setAttribute("onclick", "deleteEstacionamiento(" + resultados.id + ")");
-                celdaAcciones.appendChild(botonEliminar);
-                //fin creacion de boton de eliminar
+                
 
-                //inicio creacion de boton de Facturar
-                let botonFacturar = document.createElement('a');
-                botonFacturar.href = '#';
-                botonFacturar.classList.add('fas');
-                botonFacturar.classList.add('fa-dollar-sign');
-                botonFacturar.classList.add('pay');
-                botonFacturar.setAttribute("title", "Facturar");
-                botonFacturar.dataset.id_estacionamiento = resultados.id;
-                celdaAcciones.appendChild(botonFacturar);
-                //fin creacion de boton de Facturar
+                
 
             }
         })
@@ -91,12 +102,12 @@ async function getVehiculos() {
                 let celdaPlaca = fila.insertCell();
                 let celdaTipo = fila.insertCell();
                 let celdaAcciones = fila.insertCell();
+                
+                if(resultados.placa!=""){
+                    celdaPlaca.innerHTML = resultados.placa;
+                    celdaTipo.innerHTML = resultados.tipo;
 
-                celdaPlaca.innerHTML = resultados.placa;
-                celdaTipo.innerHTML = resultados.tipo;
-
-
-                //inicio creacion de boton de eliminar
+                    //inicio creacion de boton de eliminar
                 let botonEliminar = document.createElement('a');
                 //botonEliminar.href = '#';
                 botonEliminar.classList.add('fas');
@@ -107,6 +118,13 @@ async function getVehiculos() {
                 botonEliminar.setAttribute("onclick", "deleteVehiculo(" + resultados.id + ")");
                 celdaAcciones.appendChild(botonEliminar);
                 //fin creacion de boton de eliminar
+                }else{
+                    celdaPlaca.innerHTML = "Vacío";
+                }
+                
+
+
+                
                 
                 
             }
